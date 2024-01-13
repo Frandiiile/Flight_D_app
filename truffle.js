89b184f892a852c8545658500473d54b3ffbe6fe
Aimane Frandile <95171284+Frandiiile@users.noreply.github.com>
2024-01-13 10:00:33 +0100

    Add files via upload

---
+++ truffle.js
@@ -0,0 +1,32 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: '*'
+    },
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
+  mocha: {
+    parallel: false,
+    reporter: 'eth-gas-reporter',
+    reporterOptions : { 
+      currency: 'USD',
+      excludeContracts: ['Migrations'],
+      outputFile: 'fs-gas-report.rst',
+      rst: true,
+      rstTitle: 'Flight Surety Gas Consumption',
+      noColors: true
+    }
+  },
+  compilers: {
+    solc: {
+      version: "^0.7.6"
+    }
+  }
+};
